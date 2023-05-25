aff2a3b75fc27d61a9ec925112f49a96254c2088
marta vasallo <mvasalloperez@gmail.com>
2023-05-25 12:43:51 +0100

    models done

---
+++ app/models/bookmark.rb
@@ -0,0 +1,7 @@
+class Bookmark < ApplicationRecord
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "The movie already exists" }
+
+  belongs_to :movie
+  belongs_to :list
+end
