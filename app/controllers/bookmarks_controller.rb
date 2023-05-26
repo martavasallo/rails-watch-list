619d07039dc35502c9e6b2c547137b315b0d7319
marta vasallo <mvasalloperez@gmail.com>
2023-05-26 13:17:22 +0100

    Movie app user journey finished

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,44 @@
+class BookmarksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'lists#index'
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
